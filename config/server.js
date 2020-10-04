699aabfae17254f9a97daba29b1852c854b4f732
Jonathan Pichot <jonathan@pichot.us>
2020-10-04 17:43:11 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ee297f5dc3dc459425bc344d3ee8fa88'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '27a96fa5-a0a8-4559-b3de-ffaafab5d379'
+};
